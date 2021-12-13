fa8f3253303e00fd7d1285b8cb84a3ad764bf887
Peter Rasmussen <peter@rustos.org>
2021-12-13 11:42:34 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,7 @@ set -e
 tw_lines=""  # Lines containing trailing whitespaces.
 
 # TODO (harupy): Check only changed files.
-for file in $(git diff-tree --no-commit-id --name-only -r HEAD
-| sed -e 's/^/.\//')
+for file in $(git diff-tree --no-commit-id --name-only -r HEAD | sed -e 's/^/.\//')
 do
   lines=$(egrep -rnIH " +$" $file | cut -f-2 -d ":")
   if [ ! -z "$lines" ]; then
